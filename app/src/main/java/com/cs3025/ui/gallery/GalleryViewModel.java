5acb55df4bbba125fb6a5a1bc1ef5a7b2246797b
AllanE7450 <aboutil1@unb.ca>
2020-02-24 14:24:49 -0400

    Changed colors

--- app/src/main/java/com/cs3025/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/cs3025/ui/gallery/GalleryViewModel.java
@@ -10,7 +10,7 @@ public class GalleryViewModel extends ViewModel {
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This will be a list");
     }
 
     public LiveData<String> getText() {
