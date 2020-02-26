5cf0b1c63e8f168fe53d06e915e41e5b0d1cacec
bharriso <bharriso@unb.ca>
2020-02-26 11:34:42 -0400

    Created Layout for House selection, and created another button drawable. Code to follow.

---
+++ app/src/main/java/com/cs3025/HouseSelect.java
@@ -0,0 +1,15 @@
+package com.cs3025;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HouseSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_house_select);
+
+    }
+}
