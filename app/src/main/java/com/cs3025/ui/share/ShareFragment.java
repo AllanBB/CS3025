674b3b103ee29ef745464edd534525959ee83280
AllanE7450 <aboutil1@unb.ca>
2020-02-26 13:45:51 -0400

    Added a pending task layout

--- app/src/main/java/com/cs3025/ui/share/ShareFragment.java
+++ app/src/main/java/com/cs3025/ui/share/ShareFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -12,22 +15,43 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.cs3025.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShareFragment extends Fragment {
 
     private ShareViewModel shareViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        shareViewModel =
-                ViewModelProviders.of(this).get(ShareViewModel.class);
+        shareViewModel = ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
-        shareViewModel.getText().observe(this, new Observer<String>() {
+        ListView listView;
+        List list = new ArrayList();
+        ArrayAdapter adapter;
+        listView = root.findViewById(R.id.pending_listview);
+        list.add("Waiting for Allan to accept the task sent on\n27/02/2020");
+        list.add("Waiting for Tim to accept the task sent on\n27/01/2020");
+        list.add("Waiting for John to accept the task sent on\n23/02/2020");
+        list.add("Waiting for John to accept the task sent on\n21/02/2020");
+
+        //adapter  = new ArrayAdapter(list);
+        adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, list );
+        listView.setAdapter(adapter);
+        for(int i=0;i< list.size(); i++ ) {
+            listView.setItemChecked(i, false );
+        }
+
+        /*
+        final TextView textView = root.findViewById(R.id.text_gallery);
+        galleryViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+         */
         return root;
     }
 }
